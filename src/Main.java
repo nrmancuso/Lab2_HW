93e0e9fca51d78e944d35c622b67bfc7df699fa1
Nick Mancuso <nmancus1@dtcc.edu>
2018-09-27 11:37:07 -0400

    Added new menu system

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,200 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
 public class Main {
+
+    static ArrayBag pokedex = new ArrayBag();
+    static Scanner scan = new Scanner(System.in);
+
 
     public static void main(String[] args) {
 
-        ArrayBag pokedex = new ArrayBag();
+        char userChoice = ' ';
+        boolean fileFound = false;
 
-        Pokemon dude = new Pokemon(1, "dude");
-        Pokemon asdf = new Pokemon(2, "fgui");
-        Pokemon dfgh = new Pokemon(3, "fg");
-        Pokemon tyiu = new Pokemon(4, "df");
-        Pokemon fghj = new Pokemon(5, "er");
-        Pokemon tyui = new Pokemon(6, "43e");
-        Pokemon uioi = new Pokemon(7, "34");
+        //Greeting
+        System.out.println("Welcome to Pokedex!");
 
-        pokedex.add(dude);
-        pokedex.add(asdf);
-        pokedex.add(dfgh);
-        pokedex.add(tyiu);
-        pokedex.add(fghj);
+        //build Pokedex
+        if (buildPokedex(pokedex)) {
+            System.out.println("'pokedex.txt' was loaded succesfully!");
+            fileFound = true;
+        }
 
 
-        System.out.println(pokedex);
+        while (userChoice != 'q' && fileFound) {
 
-        pokedex.removeAndShift(0);
-        pokedex.removeAndShift(2);
-        pokedex.removeAndShift(4);
-        pokedex.removeAndShift(5);
-        pokedex.removeAndShift(1);
-        pokedex.removeAndShift(3);
-        pokedex.removeAndShift(34345);
+            System.out.println("(D)isplay Pokemon, (O)utput to file, (R)ead 'changes.txt', or (Q)uit");
+            System.out.print("~> ");
+            String input = scan.next().toLowerCase();
+            userChoice = input.charAt(0);
+
+            switch (userChoice) {
+                case 'd':
+                    System.out.println(pokedex);
+                    break;
+
+                case 'o':
+                    outputMenu();
+                    break;
+
+                case 'r':
+                    readChangeFile();
+            }
+
+        }
+
+        System.out.println("Goodbye!");
+
+    }
+
+    //Output menu
+    private static void outputMenu() {
+
+        char userChoice = ' ';
+
+        System.out.println("What type of remove would you like to use?");
+        System.out.println("(R)emove and shift, (D)elete field, or (N)ext field?");
+        System.out.print("~> ");
+
+        String input = scan.next().toLowerCase();
+        userChoice = input.charAt(0);
+
+        switch (userChoice) {
+
+            case 'r':
+                outputToFile(1);
+                break;
+
+            case 'd':
+                outputToFile(2);
+                break;
+
+            case 'n':
+                outputToFile(3);
+                break;
+
+            default:
+                System.out.println("Invalid entry!");
+                break;
+
+        }
 
 
-        System.out.println(pokedex);
+    }
 
-        pokedex.add(dude);
-        pokedex.add(asdf);
-        pokedex.add(dfgh);
-        pokedex.add(tyiu);
-        pokedex.add(fghj);
+    //Helper method to handle file input
+    private static boolean buildPokedex(ArrayBag pokedex) {
+
+        //Read from input file using scanner
+        try {
+
+            File file = new File("pokedex.txt");
+            Scanner fileReader = new Scanner(file);
+            fileReader.nextLine();
+
+            while (fileReader.hasNextLine()) {
+
+                //Build string array from parsed line
+                String[] pokemonInfoArray = fileReader.nextLine().split("\\t");
+
+                Pokemon p = buildNewPokemon(pokemonInfoArray);
+
+                //Add to bag
+                pokedex.add(p);
+            }
+        } catch (FileNotFoundException e) {
+
+            System.out.println("\nFile not found!");        //whoops
+
+        }
+        return true;
+
+    }
+
+    private static void readChangeFile() {
+
+        //Read from input file using scanner
+        try {
+
+            File file = new File("changes.txt");
+            Scanner fileReader = new Scanner(file);
+            fileReader.nextLine();
+
+            while (fileReader.hasNextLine()) {
+
+                //Build string array from parsed line
+                String[] pokemonInfoArray = fileReader.nextLine().split("\\t");
+
+                if (pokemonInfoArray[0].equalsIgnoreCase("A")) {
+
+                    //Build new pokemon
+                    Pokemon p = new Pokemon(
+                            Integer.parseInt(pokemonInfoArray[1]),  //ID number
+                            pokemonInfoArray[2]);                   //Name
+
+                    //Add to bag
+                    pokedex.add(p);
+                } else if (pokemonInfoArray[0].equalsIgnoreCase("D")) {
+
+                    pokedex.setDeleteFieldTrue(Integer.parseInt(pokemonInfoArray[1]));
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("\nFile not found!");        //whoops
+
+        }
 
 
-        pokedex.setDeleteFieldTrue(3);
-        pokedex.setDeleteFieldTrue(5);
-        pokedex.setDeleteFieldTrue(9);
+    }
 
-        System.out.println(pokedex);
+    public static Pokemon buildNewPokemon(String[] pokemonInfoArray) {
 
+        //Build new pokemon
+        Pokemon p = new Pokemon(
+                Integer.parseInt(pokemonInfoArray[0]),  //ID number
+                pokemonInfoArray[1]);                   //Name
 
-        pokedex.clear();
-
-        System.out.println("Testing clear:::");
-
-        System.out.println(pokedex);
-
-        pokedex.add(dude);
-        pokedex.add(asdf);
-        pokedex.add(dfgh);
-        pokedex.add(tyiu);
-        pokedex.add(fghj);
+        return p;
 
 
-        pokedex.setNexttoDelete();
+    }
 
-        System.out.println(pokedex);
+    private static void outputToFile(int typeOfOutput) {
 
-        pokedex.setDeleteFieldTrue(3);
-        pokedex.setDeleteFieldTrue(5);
-        pokedex.setDeleteFieldTrue(9);
+        try {
+            PrintWriter outputFile = new PrintWriter(new FileOutputStream("pokedex" + typeOfOutput + ".txt"));
 
-        pokedex.setNexttoDelete();
+            switch (typeOfOutput) {
 
-        System.out.println(pokedex);
+                case 1:
+                    outputFile.println(pokedex.toTabFileWithSkip());
+                    outputFile.flush();
+                    outputFile.close();
+                    break;
 
+                case 2:
+                    outputFile.println(pokedex.toTabFileWithDelete());
+                    outputFile.flush();
+                    outputFile.close();
+                    break;
+
+                case 3:
+                    outputFile.println(pokedex.toTabFileWithNext());
+                    outputFile.flush();
+                    outputFile.close();
+                    break;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot create file!");
+        }
 
     }
 }
